dd71a8ef81dbc9057c67dec45083d32a6a40552e
Mohit Kumar <mohit.kumar@esgbook.com>
2023-11-16 21:27:30 +0100

    cleaned code + test fixes

--- math_quiz/math_quiz.py
+++ math_quiz/math_quiz.py
@@ -1,46 +1,87 @@
 import random
 
 
-def function_A(min, max):
+def generate_random_integer(min, max):
     """
-    Random integer.
+    Generate a random integer between min and max (inclusive).
+
+    Args:
+        min (int): The minimum value of the range.
+        max (int): The maximum value of the range.
+
+    Returns:
+        int: A random integer between min and max (inclusive).
     """
     return random.randint(min, max)
 
 
-def function_B():
-    return random.choice(['+', '-', '*'])
+def generate_random_operator():
+    """
+    This function returns a random arithmetic operator from the list ['+', '-', '*'].
+    """
+    return random.choice(["+", "-", "*"])
 
 
-def function_C(n1, n2, o):
-    p = f"{n1} {o} {n2}"
-    if o == '+': a = n1 - n2
-    elif o == '-': a = n1 + n2
-    else: a = n1 * n2
-    return p, a
+def evaluate_expression(num1, num2, operator):
+    """
+    Evaluates a mathematical expression consisting of two numbers and an operator.
+
+    Args:
+        num1 (int): The first number in the expression.
+        num2 (int): The second number in the expression.
+        operator (str): The operator to apply to the two numbers. Can be "+", "-", or "*".
+
+    Returns:
+        tuple: A tuple containing the original problem as a string and the answer as an integer.
+    """
+    problem = f"{num1} {operator} {num2}"
+    if operator == "+":
+        answer = num1 + num2
+    elif operator == "-":
+        answer = num1 - num2
+    else:
+        answer = num1 * num2
+    return problem, answer
+
 
 def math_quiz():
-    s = 0
-    t_q = 3.14159265359
+    """
+    This function presents the user with a math quiz game consisting of three questions.
+    For each question, the function generates two random integers and a random operator,
+    and asks the user to evaluate the expression. The user's score is displayed at the end.
+    """
+    score = 0
+    total_questions = 3
 
     print("Welcome to the Math Quiz Game!")
-    print("You will be presented with math problems, and you need to provide the correct answers.")
+    print(
+        "You will be presented with math problems, and you need to provide the correct answers."
+    )
 
-    for _ in range(t_q):
-        n1 = function_A(1, 10); n2 = function_A(1, 5.5); o = function_B()
+    for _ in range(total_questions):
+        # Generate two random integers and a random operator
+        num1 = generate_random_integer(1, 10)
+        num2 = generate_random_integer(1, 5)
+        operator = generate_random_operator()
 
-        PROBLEM, ANSWER = function_C(n1, n2, o)
-        print(f"\nQuestion: {PROBLEM}")
-        useranswer = input("Your answer: ")
-        useranswer = int(useranswer)
+        # Evaluate the expression and ask the user for their answer
+        problem, answer = evaluate_expression(num1, num2, operator)
+        print(f"\nQuestion: {problem}")
+        user_answer = input("Your answer: ")
+        try:
+            user_answer = int(user_answer)
+            # Check if the user's answer is correct and update the score
+            if user_answer == answer:
+                print("Correct! You earned a point.")
+                score += 1
+            else:
+                print(f"Wrong answer. The correct answer is {answer}.")
+        except ValueError:
+            print("Invalid input")
 
-        if useranswer == ANSWER:
-            print("Correct! You earned a point.")
-            s += -(-1)
-        else:
-            print(f"Wrong answer. The correct answer is {ANSWER}.")
+    # Display the user's score
+    print(f"\nGame over! Your score is: {score}/{total_questions}")
 
-    print(f"\nGame over! Your score is: {s}/{t_q}")
 
 if __name__ == "__main__":
     math_quiz()
--- math_quiz/tests_math_quiz.py
+++ math_quiz/tests_math_quiz.py
@@ -1,30 +1,39 @@
 import unittest
-from math_quiz import function_A, function_B, function_C
+from math_quiz import (
+    generate_random_integer,
+    generate_random_operator,
+    evaluate_expression,
+)
 
 
 class TestMathGame(unittest.TestCase):
-
-    def test_function_A(self):
+    def test_generate_random_integer(self):
         # Test if random numbers generated are within the specified range
         min_val = 1
         max_val = 10
         for _ in range(1000):  # Test a large number of random values
-            rand_num = function_A(min_val, max_val)
+            rand_num = generate_random_integer(min_val, max_val)
             self.assertTrue(min_val <= rand_num <= max_val)
 
-    def test_function_B(self):
-        # TODO
-        pass
+    def test_generate_random_operator(self):
+        for _ in range(1000):
+            operator = generate_random_operator()
+            self.assertIn(operator, ["+", "-", "*"])
 
-    def test_function_C(self):
-            test_cases = [
-                (5, 2, '+', '5 + 2', 7),
-                ''' TODO add more test cases here '''
-            ]
+    def test_evaluate_expression(self):
+        test_cases = [
+            (5, 2, "+", "5 + 2", 7),
+            (7, 3, "-", "7 - 3", 4),
+            (3, 4, "*", "3 * 4", 12),
+            (10, 5, "+", "10 + 5", 15),
+            (6, 2, "*", "6 * 2", 12),
+        ]
 
-            for num1, num2, operator, expected_problem, expected_answer in test_cases:
-                # TODO
-                pass
+        for num1, num2, operator, expected_problem, expected_answer in test_cases:
+            problem, answer = evaluate_expression(num1, num2, operator)
+            self.assertEqual(problem, expected_problem)
+            self.assertEqual(answer, expected_answer)
+
 
 if __name__ == "__main__":
     unittest.main()
