c571327eb76117b36f836c625c491ed47ae40f1e
Mohit Kumar <mohit.kumar@esgbook.com>
2023-11-16 21:46:43 +0100

    fixing

--- setup.py
+++ setup.py
@@ -1,20 +1,20 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="math-quiz-game",
-    version="0.1.0",
+    name='math-quiz-game',
+    version='0.1.0',
     packages=find_packages(),
     install_requires=[
         # List your dependencies here if any
     ],
     entry_points={
-        "console_scripts": [
-            "math-quiz=math_quiz:math_quiz",
+        'console_scripts': [
+            'math-quiz=math_quiz.__main__:math_quiz',
         ],
     },
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Apache License 2.0',
+        'Operating System :: OS Independent',
     ],
 )
